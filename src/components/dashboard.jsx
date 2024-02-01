a8bde63a25897575884b8e83434f5475e0160f42
PonrasuThanghavel <ponrasuthanghavel@gmail.com>
2024-02-01 12:14:50 +0530

    first commit

---
+++ src/components/dashboard.jsx
@@ -0,0 +1,14 @@
+// Dashboard.js
+import React from 'react';
+import './css/dashboard.css'; // Import Dashboard CSS
+
+const Dashboard = () => {
+  return (
+    <div className="dashboard">
+      <h1>Welcome to the Dashboard</h1>
+      {/* Add your dashboard content here */}
+    </div>
+  );
+};
+
+export default Dashboard;
